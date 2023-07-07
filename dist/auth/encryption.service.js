55b55d9377442dac78d0b377cc8a305030cafd6b
Olesya <olesyam.miller@gmail.com>
2023-07-07 12:22:23 -0400

    fixes

---
+++ dist/auth/encryption.service.js
@@ -0,0 +1,49 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EncryptionService = void 0;
+const common_1 = require("@nestjs/common");
+const config_1 = require("@nestjs/config");
+const crypto = require("crypto");
+let EncryptionService = class EncryptionService {
+    constructor(configService) {
+        this.configService = configService;
+        this.algorithm = 'aes256';
+    }
+    encrypt(data) {
+        const iv = crypto.randomBytes(16);
+        const cipher = crypto.createCipheriv(this.algorithm, Buffer.concat([
+            Buffer.from(this.configService.get('ENCRYPTION_KEY')),
+            Buffer.alloc(32),
+        ], 32), iv);
+        let encrypted = cipher.update(data);
+        encrypted = Buffer.concat([encrypted, cipher.final()]);
+        return `${iv.toString('hex')}:${encrypted.toString('hex')}`;
+    }
+    decrypt(data) {
+        const textParts = data.split(':');
+        const iv = Buffer.from(textParts.shift(), 'hex');
+        const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+        const decipher = crypto.createDecipheriv(this.algorithm, Buffer.concat([
+            Buffer.from(this.configService.get('ENCRYPTION_KEY')),
+            Buffer.alloc(32),
+        ], 32), iv);
+        let decrypted = decipher.update(encryptedText);
+        decrypted = Buffer.concat([decrypted, decipher.final()]);
+        return decrypted.toString();
+    }
+};
+EncryptionService = __decorate([
+    (0, common_1.Injectable)(),
+    __metadata("design:paramtypes", [config_1.ConfigService])
+], EncryptionService);
+exports.EncryptionService = EncryptionService;
+//# sourceMappingURL=encryption.service.js.map
