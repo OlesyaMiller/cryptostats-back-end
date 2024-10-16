5daf3e5b836c4c9bde9e256e90b309049f7dd65f
Olesya <olesyam.miller@gmail.com>
2024-10-16 19:43:19 -0400

    update users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { CurrentUser } from '../auth/current-user.decorator';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CreateUserRequest } from './dto/request/create-user-request.dto';
+import { UserResponse } from './dto/response/user-response.dto';
 import { UsersService } from './users.service';
-import { CreateUserRequestDto } from './dto/request/create-user-request.dto';
-import { UserResponseDto } from './dto/response/user-response.dto';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
 
-    @Post()
-    async createUser(@Body() createUserRequestDto: CreateUserRequestDto): Promise<UserResponseDto> {
-        return this.usersService.createUser(createUserRequestDto);
-    }
+  @Post()
+  async createUser(
+    @Body() createUserRequest: CreateUserRequest,
+  ): Promise<UserResponse> {
+    return this.usersService.createUser(createUserRequest);
+  }
 
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async getUser(@CurrentUser() user: UserResponse): Promise<UserResponse> {
+    return user;
+  }
 }
